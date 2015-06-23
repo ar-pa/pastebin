6824d9dd6693d88615bea419aa0cc1cc6be989f9
amirreza poorakhavan <a.poorakhavan@gmail.com>
2015-06-23 16:40:05 +0430

    Update charpa-contest.cpp

--- charpa-contest.cpp
+++ charpa-contest.cpp
@@ -1,19 +1,19 @@
 //God & me // ya mahdi adrekni
 #include <iostream>
 using namespace std;
-int df[20000],dc[20000][20000],M=1e9+7;
-int n,c;
+int f[20000],c[20000][20000],M=1e9+7;
+int n;
 int main(){
   cin>>n;
   for(int i=1;i<=n;i++){
-    dc[i][0]=1,dc[i][i]=1;
+    c[i][0]=1,c[i][i]=1;
     for(int j=1;j<i;j++)
-      dc[i][j]=(dc[i-1][j-1]+dc[i-1][j])%M;
+      c[i][j]=(c[i-1][j-1]+c[i-1][j])%M;
   }
-  df[0]=df[1]=1;
+  f[0]=f[1]=1;
   for(int i=2;i<=n;i++)
     for(int j=0;j<i;j++)
-      df[i]+=(dc[i][i-j]*(long long int)df[j])%M,df[i]%=M;
-  cout<<df[n]<<endl;
+      f[i]+=(c[i][i-j]*(long long int)f[j])%M,f[i]%=M;
+  cout<<f[n]<<endl;
   return 0;
 }
