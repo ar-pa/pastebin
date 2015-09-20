25351e6d088b095924fa1f506e702c4cc98eb69c
amirreza poorakhavan <a.poorakhavan@gmail.com>
2015-09-20 18:50:59 +0430

    https://www.codechef.com/problems/IGCDMILK

---
+++ paste/CC_IGCDMILK.cpp
@@ -0,0 +1,38 @@
+//God & me // ya mahD adrekni 
+//mishe ye tala ham be ma bedan?      (En commentam loose?)
+#include <bits/stdc++.h>
+#define push(a,b,c,y) (flag[a][b][c]?():(qu[size].a=a,qu[size].b=b,qu[size].c=c,flag[a][b][c]=true,qu[size].par=pos,q[size++].d=y))
+#define A qu[pos].a
+#define B qu[pos].b
+#define C qu[pos].c
+using namespace std;
+char t,a,b,c,g;
+bool flag[101][101][101];
+struct h{
+  char a,b,c,d;
+  int par;
+  inline bool ok(){
+    return (a==g && (b==g || c==g)) || (b==g && c==g);
+  }
+}qu[1000012];
+inline void bfs(){
+  int size=1,pos=0;
+  if()
+  qu[pos]={a,b,c,0,-1};
+  flag[a][b][c]=true;
+  while(pos!=size){
+    push(min(a,))
+    pos++;
+  }
+}
+int main(){
+  ios_base::sync_with_stdio(0);
+  cin>>t;
+  while(t--){
+    memset(flag,0,sizeof(flag[0][0][0])*1030301);
+
+  }
+  return 0;
+}
+//be omide khoda!
+//khoda ya shokret! cheqad to be man lotf dari :)
